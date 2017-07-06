2177936bd217298f2ede4917fe5b39baae917383
7kasper <kasper77077@gmail.com>
2017-07-06 20:05:51 +0200

    Yay, I found the way. Implemented and fixed leashing objects aswell o_O

--- src/nl/dev7kasper/NukkitTester/main/NukkitTester.java
+++ src/nl/dev7kasper/NukkitTester/main/NukkitTester.java
@@ -4,10 +4,12 @@ import cn.nukkit.Player;
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
 import cn.nukkit.entity.Entity;
+import cn.nukkit.entity.EntityLiving;
 import cn.nukkit.entity.data.LongEntityData;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.server.ServerCommandEvent;
+import cn.nukkit.network.protocol.EntityEventPacket;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.TextFormat;;
 
@@ -37,9 +39,11 @@ public class NukkitTester extends PluginBase implements Listener {
                     	Player p = (Player) s;
                     	s.sendMessage("Leashing the sh*t outta everyone.");
                     	for(Entity e : p.getLevel().getEntities()) {
-                    		e.setDataFlag(Entity.DATA_FLAGS, Entity.DATA_FLAG_LEASHED, true);
-                    		e.setDataProperty(new LongEntityData(Entity.DATA_LEAD_HOLDER_EID, p.getId()));
-                    		s.sendMessage("Leashed " + Long.toString(e.getId()) + " to " + p.getId() + "!");
+                    		if(e instanceof EntityLiving) {
+                        		e.setDataFlag(Entity.DATA_FLAGS, Entity.DATA_FLAG_LEASHED, true);
+                        		e.setDataProperty(new LongEntityData(Entity.DATA_LEAD_HOLDER_EID, p.getId()));
+                        		s.sendMessage("Leashed " + Long.toString(e.getId()) + " to " + p.getId() + "!");
+                    		}
                     	}
                     } else {
                     	s.sendMessage(TextFormat.DARK_RED + "[Error] Whattcha doing not being a player?");
@@ -57,45 +61,72 @@ public class NukkitTester extends PluginBase implements Listener {
                     			case "toself":
                         			s.sendMessage("Unleashing the sh*t outta everyone, selfy style.");
                                 	for(Entity e : p.getLevel().getEntities()) {
-                                		e.setDataProperty(new LongEntityData(Entity.DATA_LEAD_HOLDER_EID, e.getId()));
-                                		s.sendMessage("(Tried) Unleashing " + Long.toString(e.getId()) + " to " + e.getId() + "!");
+                                		if(e instanceof EntityLiving) {
+	                                		e.setDataProperty(new LongEntityData(Entity.DATA_LEAD_HOLDER_EID, e.getId()));
+	                                		s.sendMessage("(Tried) Unleashing " + Long.toString(e.getId()) + " to " + e.getId() + "!");
+                                		}
                                 	}
                     			return true;
                     			case "respawn":
                         			s.sendMessage("Unleashing the sh*t outta everyone, respawn-style.");
                                 	for(Entity e : p.getLevel().getEntities()) {
-                                		e.respawnToAll();
-                                		s.sendMessage("(Tried) Unleashing " + Long.toString(e.getId()) + " by respawning?");
+                                		if(e instanceof EntityLiving) {
+                                    		e.respawnToAll();
+                                    		s.sendMessage("(Tried) Unleashing " + Long.toString(e.getId()) + " by respawning?");
+                                		}
                                 	}
                 				return true;
                     			case "zero":
                     			case "0":
                         			s.sendMessage("Unleashing the sh*t outta everyone, " + TextFormat.BLACK + "Zorro" + TextFormat.RESET + " style.");
                                 	for(Entity e : p.getLevel().getEntities()) {
-                                		e.setDataFlag(Entity.DATA_FLAGS, Entity.DATA_FLAG_LEASHED, false);
-                                		e.setDataProperty(new LongEntityData(Entity.DATA_LEAD_HOLDER_EID, 0));
-                                		s.sendMessage("(Tried) Unleashing " + Long.toString(e.getId()) + " by zeroing.");
+                                		if(e instanceof EntityLiving) {
+	                                		e.setDataFlag(Entity.DATA_FLAGS, Entity.DATA_FLAG_LEASHED, false);
+	                                		e.setDataProperty(new LongEntityData(Entity.DATA_LEAD_HOLDER_EID, 0));
+	                                		s.sendMessage("(Tried) Unleashing " + Long.toString(e.getId()) + " by zeroing.");
+                                		}
                                 	}
                 				return true;
                     			case "chuck":
                     				if(args.length > 1 && args[1].equalsIgnoreCase("norris")) {
                             			s.sendMessage("Unleashing the sh*t outta everyone, " + TextFormat.YELLOW + "Chuck Norris" + TextFormat.RESET + " style!");
                                     	for(Entity e : p.getLevel().getEntities()) {
-                                    		e.setDataFlag(Entity.DATA_FLAGS, Entity.DATA_FLAG_LEASHED, false);
-                                    		e.setDataProperty(new LongEntityData(Entity.DATA_LEAD_HOLDER_EID, -1));
-                                    		e.despawnFromAll();
-                                    		e.spawnToAll();
+                                    		if(e instanceof EntityLiving) {
+	                                    		e.setDataFlag(Entity.DATA_FLAGS, Entity.DATA_FLAG_LEASHED, false);
+	                                    		e.setDataProperty(new LongEntityData(Entity.DATA_LEAD_HOLDER_EID, -1));
+	                                    		e.despawnFromAll();
+	                                    		e.spawnToAll();
+                                    		}
                                     	}
                                     	return true;
                     				}
                     				break;
+                    			case "-1":
+                                	s.sendMessage("Unleashing the sh*t outta everyone.");
+                                	for(Entity e : p.getLevel().getEntities()) {
+                                		if(e instanceof EntityLiving) {
+	                                		e.setDataFlag(Entity.DATA_FLAGS, Entity.DATA_FLAG_LEASHED, false);
+	                                		e.setDataProperty(new LongEntityData(Entity.DATA_LEAD_HOLDER_EID, -1));
+	                                		s.sendMessage("(Tried) Unleashing " + Long.toString(e.getId()) + " from " + p.getId() + "!");
+                                		}
+                                	}
+                    			break;
                     		}
                     	}
                     	s.sendMessage("Unleashing the sh*t outta everyone.");
                     	for(Entity e : p.getLevel().getEntities()) {
-                    		e.setDataFlag(Entity.DATA_FLAGS, Entity.DATA_FLAG_LEASHED, false);
-                    		e.setDataProperty(new LongEntityData(Entity.DATA_LEAD_HOLDER_EID, -1));
-                    		s.sendMessage("(Tried) Unleashing " + Long.toString(e.getId()) + " from " + p.getId() + "!");
+                    		if(e instanceof EntityLiving) {
+                        		e.setDataFlag(Entity.DATA_FLAGS, Entity.DATA_FLAG_LEASHED, false);
+                        		e.setDataProperty(new LongEntityData(Entity.DATA_LEAD_HOLDER_EID, -1));
+                        		EntityEventPacket packet = new EntityEventPacket();
+                        		packet.eid = e.getId();
+                        		packet.event = 63; //Baked straight in IDA's delicious oven.
+                        		packet.unknown = 0;
+                                for (Player aPlayer : p.getLevel().getPlayers().values()) {
+                                    aPlayer.dataPacket(packet);
+                                }
+                    		}
+                    		s.sendMessage("Unleashed " + Long.toString(e.getId()) + " from " + p.getId() + "!");
                     	}
                     } else {
                     	s.sendMessage(TextFormat.DARK_RED + "[Error] Whattcha doing not being a player?");
